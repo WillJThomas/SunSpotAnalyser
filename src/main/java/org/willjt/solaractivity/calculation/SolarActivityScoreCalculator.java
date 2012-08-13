2efee7905089b5672c7e7041e70389f6f41217d6
Will Thomas <withomas214@gmail.com>
2012-08-14 00:05:29 +0100

    Extracted some private methods to make SolarActivityScoreCalculator class easier to read

--- src/main/java/org/willjt/solaractivity/calculation/SolarActivityScoreCalculator.java
+++ src/main/java/org/willjt/solaractivity/calculation/SolarActivityScoreCalculator.java
@@ -8,43 +8,66 @@ public class SolarActivityScoreCalculator {
     }
 
     public int scoreForLocation(int column, int row) {
-        if (column < 0 || row < 0) {
-            throw new IllegalArgumentException("Both column and row number of location must both be at least 0.");
+        if (locationIsNotWithinGridBounds(column, row)) {
+            throw new IllegalArgumentException("This (column,row) location is not within the grid bounds: (" + column + "," + row + ")");
         }
-        if (column >= measurementsGrid.length || row >= measurementsGrid[0].length) {
-            throw new IllegalArgumentException("This location is out of the grid bounds: " + column + "," + row);
-        }
+        ;
 
-        int locationScore = 0;
+        int indexOfFirstColumnToCount = indexMinusOneOrIndexIfZero(column);
+        int indexOflastColumnToCount = indexPlusOneOrIndexIfLastColumn(column);
+        int indexOfFirstRowToCount = indexMinusOneOrIndexIfZero(row);
+        int indexOfLastRowToCount = indexPlusOneOrIndexIfLastRow(row);
 
-        int indexOfFirstColumnToCount = column - 1;
-        int indexOfFirstRowToCount = row - 1;
-
-        while (indexOfFirstColumnToCount < 0) {
-            indexOfFirstColumnToCount++;
-        }
-
-        while (indexOfFirstRowToCount < 0) {
-            indexOfFirstRowToCount++;
-        }
-
-        int indexOflastColumnToCount = column + 1;
-        int indexOfLastRowToCount = row + 1;
-
-        while (indexOflastColumnToCount >= measurementsGrid.length) {
-            indexOflastColumnToCount--;
-        }
-
-        while (indexOfLastRowToCount >= measurementsGrid[0].length) {
-            indexOfLastRowToCount--;
-        }
-
-        for (int columnIndex = indexOfFirstColumnToCount; columnIndex <= indexOflastColumnToCount; columnIndex++) {
-            for (int rowIndex = indexOfFirstRowToCount; rowIndex <= indexOfLastRowToCount; rowIndex++) {
-                locationScore += measurementsGrid[columnIndex][rowIndex];
-            }
-        }
+        int locationScore = sumOfMeasurementsInGridArea(indexOfFirstColumnToCount, indexOflastColumnToCount, indexOfFirstRowToCount, indexOfLastRowToCount);
 
         return locationScore;
+    }
+
+    private int sumOfMeasurementsInGridArea(int indexOfFirstColumn, int indexOflastColumn, int indexOfFirstRow, int indexOfLastRow) {
+        int sum = 0;
+
+        for (int columnIndex = indexOfFirstColumn; columnIndex <= indexOflastColumn; columnIndex++) {
+            for (int rowIndex = indexOfFirstRow; rowIndex <= indexOfLastRow; rowIndex++) {
+                sum += measurementsGrid[columnIndex][rowIndex];
+            }
+        }
+        return sum;
+    }
+
+    private int indexPlusOneOrIndexIfLastRow(int rowIndex) {
+        int indexToUse = rowIndex + 1;
+        while (indexToUse == measurementsGrid[0].length) {
+            indexToUse--;
+        }
+        return indexToUse;
+    }
+
+    private int indexPlusOneOrIndexIfLastColumn(int columnIndex) {
+        int indexToUse = columnIndex + 1;
+        if (indexToUse == measurementsGrid.length) {
+            indexToUse--;
+        }
+        return indexToUse;
+    }
+
+    private int indexMinusOneOrIndexIfZero(int index) {
+        int indexToUse = index - 1;
+
+        if (indexToUse < 0) {
+            indexToUse++;
+        }
+
+        return indexToUse;
+    }
+
+    private boolean locationIsNotWithinGridBounds(int column, int row) {
+        if (column < 0 || row < 0) {
+            return true;
+        }
+        if (column >= measurementsGrid.length || row >= measurementsGrid[0].length) {
+            return true;
+        }
+
+        return false;
     }
 }
